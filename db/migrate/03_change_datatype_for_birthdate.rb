ed35f7ca9a66297ad775e9921619432ceb0bf75d
fidelscodes <fidelscodes@gmail.com>
2016-08-01 12:07:28 -0400

    Update with solution

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,2 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+  def change
+    # Notice that all arguments are keyword arguments
+    change_column :students, :birthdate, :datetime
+  end
 end
